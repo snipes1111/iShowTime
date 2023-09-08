4ba3c3e1613a8be77538de2ebffc45a2f74b2146
snipes1111 <km-92@mail.ru>
2023-09-08 18:49:39 +0200

    Setup tabbar item

--- iShowTime/MainTabBarModule/MainTabBarController.swift
+++ iShowTime/MainTabBarModule/MainTabBarController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 
+    let builder = NavVCBuilder()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setViewControllers()
@@ -17,48 +19,10 @@ class MainTabBarController: UITabBarController {
 }
 
 extension MainTabBarController {
-    private func createNavController(viewController: UIViewController,
-                                     title: String,
-                                     image: UIImage?) -> UIViewController {
-        let navController = UINavigationController(rootViewController: viewController)
-        navController.tabBarItem.image = image
-        navController.tabBarItem.title = title
-        navController.tabBarItem.titlePositionAdjustment = .init(horizontal: 0, vertical: 6)
-        navController.tabBarItem.setTitleTextAttributes(Fonts.tabItemTextAttributes, for: .normal)
-        navController.navigationBar.prefersLargeTitles = true
-        viewController.navigationItem.title = title
-        return navController
-    }
-
-    private func setTabBarAppearence() {
-        let positionOnX: CGFloat = 10
-        let positionOnY: CGFloat = 16
-
-        let width = tabBar.bounds.width - positionOnX * 2
-        let height = tabBar.bounds.height + positionOnY * 2
-
-        let roundLayer = CAShapeLayer()
-
-        let bezierPath = UIBezierPath(roundedRect: CGRect(
-            x: positionOnX,
-            y: tabBar.bounds.minY - positionOnY,
-            width: width,
-            height: height), cornerRadius: height / 2)
-
-        roundLayer.path = bezierPath.cgPath
-
-        tabBar.layer.insertSublayer(roundLayer, at: 0)
-
-        tabBar.tintColor = .systemPink
-    }
 
     private func setViewControllers() {
         viewControllers = [
-            createNavController(
-                viewController: SearchSeriesViewController(),
-                title: SearchModuleHelpers.Constants.title.rawValue,
-                image: Images.imageForTabItem(.search)
-            )
+            builder.createSearchVc()
         ]
     }
 }
