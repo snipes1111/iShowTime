b1711fc113c3d07e33a96a96dc42285079901275
snipes1111 <km-92@mail.ru>
2023-09-08 14:08:18 +0200

    Create SearchVC and SearchView files

--- iShowTime/SearchModule/ViewControllers/SearchSeriesVC.swift
+++ iShowTime/SearchModule/ViewControllers/SearchSeriesVC.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let searchView = SearchSeriesView()
+        self.view = searchView
+        
     }
-
-
+    
 }
-
---
+++ iShowTime/SearchModule/Views/SearchSeriesView.swift
@@ -0,0 +1,23 @@
+//
+//  SearchSeriesView.swift
+//  iShowTime
+//
+//  Created by user on 08/09/2023.
+//
+
+import UIKit
+
+class SearchSeriesView: UIView {
+    
+    var color: UIColor?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .gray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
